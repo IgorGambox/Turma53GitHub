21a4584f2330f7f3ffd71739e05562c46bb9966d
Igor <igor.gambox@gmail.com>
2022-05-10 11:53:18 -0300

    Exercicios Java Lacos de Repeticao2

---
+++ Turma53GitHub/Bloco I/Java/Introducao/src/Introducao/Exemplo.java
@@ -0,0 +1,23 @@
+package Introducao;
+
+import java.util.Scanner;
+
+public class Exemplo {
+
+	public static void main(String[] args) {
+	Scanner ler = new Scanner(System.in);
+	int n;
+	System.out.println("Tabuada do 4\n)");
+	
+	for(int x=1;x<10;x++)
+		System.out.println( "4 X "+x+" = " +4*x);
+	
+	System.out.println("\nEscolha uma TABUADA: ");
+	n=ler.nextInt();
+	for(int x=1;x<=10;x++);
+	
+	
+
+	}
+
+}
---
+++ Turma53GitHub/Bloco I/Java/Introducao/src/Introducao/Repeticao2.java
@@ -0,0 +1,40 @@
+package Introducao;
+
+import java.util.Scanner;
+
+public class Repeticao2 {
+
+	public static void main(String[] args) 
+	{
+		Scanner entrada = new Scanner (System.in);
+		
+		int idade;
+		
+		System.out.printf("\nEntre com a sua idade ");
+		idade = entrada.nextInt();
+		
+		while(idade>=1)
+		{
+			System.out.printf("Sua idade: %d",idade);
+			if(idade>=18);
+			{
+				System.out.printf("Voce e de maior... ");
+			}
+	
+			else
+		
+		}
+		
+	}
+			
+				
+			
+		}
+
+
+		
+		
+	
+		
+	
+	
